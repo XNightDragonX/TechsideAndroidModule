d6b082a1483a6b74d6f3e1509f98b7c4d5737239
Emanuel Pérez Solano <XNightDragonX@outlook.com>
2022-03-27 22:27:43 -0600

    2 xml bien hechos

---
+++ app/src/main/java/com/techside/techsideandroid/Porfile.java
@@ -0,0 +1,14 @@
+package com.techside.techsideandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Porfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_porfile);
+    }
+}
